01571163442792209ff2ef844c602b0f34eca3c8
bandeep20001 <bandeep2000@gmail.com>
2023-11-19 18:26:14 -0800

    added  readme

--- use-dep/main.go
+++ use-dep/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/bandeep2000/usego/greetings/fileutils"
 	"github.com/bandeep2000/usego/greetings/hello"
 )
 
 func main() {
 
 	fmt.Println(hello.Sayhello())
+	file1 := fileutils.New("p2.txt")
+
+	//fmt.Println(file)
+	fmt.Println(file1.CheckForStringInFile("blah"))
 
 }
