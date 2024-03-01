2c4a88162aef62882f3b4083cf8ee7f530a21d59
bandeep20001 <bandeep2000@gmail.com>
2024-02-29 22:29:01 -0800

    added etcdtl commands

---
+++ gke/etcdcmd/commands.go
@@ -0,0 +1,17 @@
+package etcdcmd
+
+func getcmds() []string {
+
+	cmds := []string{
+		"k get all --all-namespaces -o yaml > all-service.yaml # back up everything",
+		"export ETCDCTL_API=3",
+		"take back up of data directory # one way to back up",
+		"ETCDCTL_API=3 etcdctl snapshot save snap.db",
+		"ETCDCTL_API=3 etcdctl snapshot status snap.db",
+		"stop api server to restore, as api server depends on it",
+		"ETCDCTL_API=3 etcdctl snapshot restore snap.db --data-dir /var/example # etcd will shutdown cluster to prevent anyone joining",
+		"Updated the etcdctl server start parameters with new data dir",
+	}
+
+	return cmds
+}
